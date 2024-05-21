4b45b6fe479cc4a515f79586ad9956af35aa4669
Brian McNestry <brianmcnestry@gmail.com>
2024-05-21 12:24:57 +0100

    prettier eslint liquid

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,27 @@
-module.exports = function(config) {
-    config.addPassthroughCopy({ public: './' })
-  
-    config.setBrowserSyncConfig({
-      files: ['dist/**/*'],
-    })
-  
-    return {
-      templateFormats: ['md', 'njk', 'jpg', 'png', 'gif'],
-      dir: {
-        input: 'src',
-        output: '_site',
-        layouts: '_js/layouts',
-      }
+const CleanCSS = require("clean-css");
+
+module.exports = function (config) {
+  config.addPassthroughCopy({ public: './' });
+
+  config.setBrowserSyncConfig({
+    files: ['build/**/*'],
+  });
+
+  module.exports = function (eleventyConfig) {
+	eleventyConfig.setLiquidOptions({
+		jsTruthy: true,
+	});
+  };
+
+  config.addFilter('cssmin', (code) => new CleanCSS({}).minify(code).styles);
+
+  config.addPassthroughCopy('assets');
+
+  return {
+    templateFormats: ['njk', 'jpg', 'png', 'gif'],
+    dir: {
+      input: 'src',
+      output: 'build',
     }
+  }
 }
